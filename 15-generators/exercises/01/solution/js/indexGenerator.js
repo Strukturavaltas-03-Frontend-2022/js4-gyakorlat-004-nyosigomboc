fd9584e5d5ca7a2b3bc2ea05cb9ad1c1584b8fe4
Gyozo Miklos <nyosdroid@gmail.com>
2022-09-09 07:10:41 +0200

    done

--- 15-generators/exercises/01/solution/js/indexGenerator.js
+++ 15-generators/exercises/01/solution/js/indexGenerator.js
@@ -1,1 +1,9 @@
+function* indexGenerator() {
+  let index = 1;
+  while (true) {
+    yield index;
+    index += 1;
+  }
+}
+
 export default indexGenerator;
--- 15-generators/exercises/02/solution/js/powersOfTwo.js
+++ 15-generators/exercises/02/solution/js/powersOfTwo.js
@@ -1,1 +1,11 @@
+function* powersOfTwo() {
+  let currentPower = 2;
+  while (true) {
+    yield currentPower;
+    // eslint-disable-next-line no-bitwise
+    // currentPower <<= 1; // really, ESlint?
+    currentPower *= 2;
+  }
+}
+
 export default powersOfTwo;
